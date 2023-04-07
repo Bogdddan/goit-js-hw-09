d28cf9ff88dd905c4354e9c5ba6846c0c586626f
Bogdddan <bogdankrajnik95@gmail.com>
2023-04-07 16:51:48 +0300

    y

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,5 @@
 // встановлюємо дату за замовчуванням
-var countDownDate = new Date().getTime();
+const countDownDate = new Date().getTime();
 
 // відображаємо календар на сторінці
 flatpickr("#calendar", {
@@ -11,19 +11,19 @@ flatpickr("#calendar", {
     countDownDate = selectedDates[0].getTime();
 
     // оновлюємо таймер кожну секунду
-    var x = setInterval(function() {
+    const x = setInterval(function() {
 
       // отримуємо поточну дату та час
-      var now = new Date().getTime();
+      const now = new Date().getTime();
 
       // знаходимо різницю між поточним часом та датою, до якої ми відлікуємо
-      var distance = countDownDate - now;
+      const distance = countDownDate - now;
 
       // обраховуємо час в днях, годинах, хвилинах та секундах
-      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
       // виводимо результат в елемент з id="timer"
       document.getElementById("timer").innerHTML = days + " днів " + hours + " годин " + minutes + " хвилин " + seconds + " секунд ";
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,14 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+  import flatpickr from "flatpickr";
+  import "flatpickr/dist/flatpickr.min.css";
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+
+
+
